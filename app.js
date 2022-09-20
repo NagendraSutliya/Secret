d1f2cbb22700f0a057da8db16724f138d69d14f8
NagendraSutliya <108868292+NagendraSutliya@users.noreply.github.com>
2022-09-20 13:00:35 +0530

    Add Environment vars
    Add Environment vars to keep secret safe.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -21,8 +22,8 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "Thisisourlittlescret.";
-userSchema.plugin(encrypt, {secret: secret, encryptrdFields: ['password']});
+console.log(process.env.SECRET);
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
